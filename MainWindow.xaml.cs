7d98152c3ea4a32a49ff716b443b10ee3faaf1d4
R.A. Nagy <r.a.nagy@gmail.com>
2020-10-31 06:51:40 -0400

    Graphical Todo Messages

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -30,6 +30,67 @@ namespace QuestKeep
         private void OnNew(object sender, RoutedEventArgs e)
         {
             QuestKeep.QuestDB.MainTest(null);
+            MessageBox.Show("Testing Success.", "OnNew");
+        }
+
+        private void OnOpen(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("TODO.", "OnOpen");
+        }
+
+        private void OnSaveAs(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("TODO.", "OnSaveAs");
+        }
+
+        private void OnAbout(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("TODO.", "OnAbout");
+        }
+
+        private void OnTestRandom(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("TODO.", "OnTestRandom");
+        }
+
+        private void OnTestOrdered(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("TODO.", "OnTestOrdered");
+        }
+
+        private void OnDump(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("TODO.", "OnDump");
+        }
+
+        private void OnDumpMerge(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("TODO.", "OnDumpMerge");
+        }
+
+        private void OnDumpRestore(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("TODO.", "OnDumpRestore");
+        }
+
+        private void OnFileReport(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("TODO.", "OnFileReport");
+        }
+
+        private void OnCopy(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("TODO.", "OnCopy");
+        }
+
+        private void OnPaste(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("TODO.", "OnPaste");
+        }
+
+        private void OnTestMetrics(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("TODO.", "OnTestMetrics");
         }
     }
 }
