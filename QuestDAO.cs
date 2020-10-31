5f781ce1621a80a30aa8e06a4b911df20cc194e0
R.A. Nagy <r.a.nagy@gmail.com>
2020-10-31 05:16:10 -0400

    .NET Core: Database assured.

---
+++ QuestDAO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+// https://www.nuget.org/packages/System.Data.SQLite.Core/
+// "updated 2 days ago"
+// NuGet CLI:
+// dotnet add package System.Data.SQLite.Core --version 1.0.113.5
+using System.Data.SQLite;
+
+
+namespace QuestKeep
+{
+    class QuestDAO
+    {
+        static bool TestConnect()
+        {
+            SQLiteConnection conn = new SQLiteConnection("QuestKeep001.bin");
+            conn.Open();
+            SQLiteCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "CREATE TABLE TEST {INT ID};";
+            cmd.ExecuteNonQuery();
+            return true;
+        }
+
+        static void Main(String[] args)
+        {
+            QuestDAO.TestConnect();
+        }
+    }
+
+}
